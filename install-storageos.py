6a3677729f71438fcc93b7c906101e547090664c
Anders Gorboe <gorboe96@gmail.com>
2021-04-30 12:20:46 +0000

    minor changes

--- install-storageos.py
+++ install-storageos.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     subprocess.run('helm repo add storageos https://charts.storageos.com', shell=True)
     subprocess.run('helm repo update', shell=True)
     subprocess.run('kubectl create namespace storageos', shell=True)
-    subprocess.run('helm install storageos storageos/storageos --namespace=storageos --set cluster.join="master-node\,worker01\,worker02"', shell=True)
+    subprocess.run('helm install storageos storageos/storageos --namespace=storageos --set cluster.join="master01\,worker01\,worker02"', shell=True)
     subprocess.run('ClusterIP=$(kubectl get svc/storageos --namespace storageos -o custom-columns=IP:spec.clusterIP --no-headers=true)', shell=True)
     subprocess.run('ApiAddress=$(echo -n "tcp://$ClusterIP:5705" | base64)', shell=True)
     subprocess.run('kubectl patch secret/storageos-api --namespace storageos --patch "{\"data\": {\"apiAddress\": \"$ApiAddress\"}}"', shell=True)
