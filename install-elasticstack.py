b16b41976041f9457e139713d7954519cec5fca4
Anders Gorboe <gorboe96@gmail.com>
2021-05-05 10:34:09 +0000

    added a sleep to the elasticsearch install script to make it install properly

--- install-elasticstack.py
+++ install-elasticstack.py
@@ -1,10 +1,12 @@
 import subprocess
+import time
 
 if __name__ == '__main__':
     subprocess.run('helm repo add elastic https://helm.elastic.co', shell=True)
     subprocess.run('helm repo update', shell=True)
     subprocess.run('kubectl create namespace logging', shell=True)
     subprocess.run('helm install elasticsearch elastic/elasticsearch -n logging', shell=True)
+    time.sleep(240)
     subprocess.run('helm install kibana elastic/kibana -n logging', shell=True)
     subprocess.run('helm install metricbeat elastic/metricbeat -n logging', shell=True)
     subprocess.run('helm install filebeat elastic/filebeat -n logging', shell=True)
